fd812befbcbc5531c3f3b6ee19712e70b200b1ab
yashhhh18 <yashhhwhat@gmail.com>
2025-11-23 22:26:34 +0530

    add

---
+++ lab3/lab 3 b/MultilevelInheritance.java
@@ -0,0 +1,29 @@
+class Vehicle {
+    String brand = "Ford";
+    public void msg() {
+        System.out.println("method form vehicle class");
+    }
+}
+
+class Car extends Vehicle {
+    String model = "Mustang";
+    public void drive() {
+        System.out.println("The car is driving.");
+    }
+}
+
+class SportsCar extends Car {
+    int maxSpeed = 300;
+
+    
+}
+
+public class MultilevelInheritance{
+ public static void main(String[] args) {
+        SportsCar mySportsCar = new SportsCar();
+        
+        mySportsCar.msg();         
+        mySportsCar.drive();        
+        System.out.println(mySportsCar.brand + " " + mySportsCar.model + " " + mySportsCar.maxSpeed);
+    }   
+}
